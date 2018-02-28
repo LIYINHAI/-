d182c139a10df31a6311fecca09848d0edb28e34
acer <acer@DESKTOP-9OJQ4LU>
2018-02-28 10:56:08 +0800

    mvc

---
+++ mvc/mvc/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public string Index()
+        {
+            return "This is my <b>default<b> action..";
+        }
+        public string Welcome(string name,int numTimes=1)
+        {
+            return HttpUtility.HtmlAttributeEncode("Hello" + name + ",NumTimes is:" + numTimes);
+        }
+    }
+}
