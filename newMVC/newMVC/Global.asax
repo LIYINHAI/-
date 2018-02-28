2a941bbee4492f7e41f85bd289f98d5fbe0b3719
acer <acer@DESKTOP-9OJQ4LU>
2018-02-28 09:37:14 +0800

    MVC

---
+++ newMVC/newMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="newMVC.MvcApplication" Language="C#" %>
---
+++ newMVC/newMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(newMVC.Startup))]
+namespace newMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
