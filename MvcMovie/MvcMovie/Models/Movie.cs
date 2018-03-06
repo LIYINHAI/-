b3f1a3e8210ebe18367cea1205fe7a7ddb2c50ff
hello-world <36828623+LIYINHAI@users.noreply.github.com>
2018-03-06 09:12:53 +0800

    Update Movie.cs
    加代码

--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -10,6 +10,7 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set;}
+        public string Context{get;set;}
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
